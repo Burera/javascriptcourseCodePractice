2bc5a0863145b4355065b8d4b40c296e5412d4ef
Burera <burerakhan4@gmail.com>
2020-06-29 23:19:12 -0700

    Add course practice

---
+++ CLass01/App.js
@@ -0,0 +1,136 @@
+console.log("class01")
+
+// Variable:
+// JavaScript includes variables which hold the data value and it can be changed anytime
+
+// Const:
+// The const declaration creates a read-only reference to a value. It does not mean the value it holds is 
+// immutable—just that the variable identifier cannot be reassigned.
+// A constant cannot share its name with a function or a variable in the same scope.
+//const mai further redeclare nhe kr skte
+// Let:
+// The let statement declares a block-scoped local variable
+// let allows you to declare variables that are limited to the scope of a block statement, or expression on which it is used, unlike the var keyword, which defines a variable globally, 
+// or locally to an entire function regardless of block scope. 
+
+
+var fname = "burera"; //string
+fname="biya";
+var number = 66;      //number
+var channel;
+console.log(fname,channel);
+
+
+// Rules of Javascript
+// 1.cannot start with numbers:
+// 2.Can start with letter ,_,$
+// 3.Are case sensitive
+
+
+// Now many developer community use let and const instead of var ...SVGAnimatedNumberList.because they both inscres the code readibility
+
+// const lname ="burera";
+// lname="biya";   ///wrongggg
+// const fruit; //error must asssign an value
+
+
+
+// global scope mean pure code ko excess kr skte hn block mtlb ek block pr km hota ha jese function k  andhr ya if-else mai
+//agr local pr variable ha he nhe tou wo global wala he le ga
+
+var city = 'Delhi';
+console.log(city);
+
+const ownersName = 'Hari Ram';
+// ownersName = 'Harry'; // Cannot do this (error)
+console.log(ownersName);
+const fruit = 'Orange';
+
+{
+ let city = 'Rampur';   
+ city = 'Kolkata';
+ console.log(city);
+}
+console.log(city);
+
+
+const arr1 = [12,23,12,53, 3];
+// arr1.push(45);
+console.log(arr1)
+
+/* Most common programming case types:
+
+1. camelCase  
+2. kebab-case
+3. snake_case 
+4. PascalCase 
+
+
+*/
+
+// Date types in js:
+// Primitive datatypes:
+// Reference Data Types:
+
+
+// Primitive:
+// String
+// number
+// Boolean
+// null
+// undefined
+// Symbol
+
+// Reference DataTypes:
+// Arrays 
+// Object literals:
+// function
+// Dates
+// // Primitive data types
+
+// // String
+// let name = "harry";
+// console.log("My string is " + name);
+// console.log("Data type is " + (typeof name));
+
+// // Numbers
+// let marks = 34.4;
+// console.log("Data type is " + (typeof marks));
+
+// // Boolean
+// let isDriver = true;
+// console.log("Data type is " + (typeof isDriver));
+
+// // Null
+// let nullVar = null;
+// console.log("Data type is " + (typeof nullVar));
+
+// // Undefined
+// let undef = undefined;
+// console.log("Data type is " + (typeof undef));
+
+// // Reference Data Types
+
+// // Arrays
+// let myarr = [1, 2, 3, 4, false, "string"];
+// console.log("Data type is " + (typeof myarr));
+
+// // Object Literals
+// let stMarks = {
+//     harry: 89,
+//     Shubham: 36,
+//     Rohan: 34
+// }
+// console.log( typeof stMarks);
+
+// function findName() {
+    
+// }
+// console.log( typeof findName);
+
+// let date = new Date();
+// console.log( typeof date);
+
+
+
+
---
+++ Class02/App.js
@@ -0,0 +1,98 @@
+// Type conversion 
+// console.log('Welcome to tut5');
+
+   let myVar = 32;
+   console.log("Data type is " +(typeof myVar))
+
+
+// let myVar;
+// myVar = String(34);
+// // console.log(myVar, (typeof myVar));
+
+let booleanVar = true;
+console.log(booleanVar, (typeof booleanVar));
+
+
+// let booleanVar = String(true);
+// // console.log(booleanVar, (typeof booleanVar));
+
+ 
+let date = new Date();
+console.log(date, (typeof date));
+
+
+// let date = String(new Date());
+// // console.log(date, (typeof date));
+
+
+ let arr = [1,2,3,4];
+ console.log(arr.length , (typeof arr)) 
+
+// let arr =  String([1,2,3,4,5]);
+// // console.log(arr.length, (typeof arr));
+
+
+
+// let i = 75;
+// // console.log(i.toString())
+
+// let stri = Number("3434");
+// stri = Number("343d4");
+// stri = Number(false);
+// stri = Number([1,2,3,4,5,6,7,8,9]);
+// // console.log(stri, (typeof stri));
+
+// let number = parseFloat('34.098');
+
+
+// console.log(number.toFixed(2), (typeof number));
+
+// // Type coercion
+
+// let mystr = Number("698");
+// let mynum = 34;
+
+// console.log(mystr + mynum);
+
+
+
+
+// console.log('We are at tut 6');
+// const name = 'Harry';
+// const greeting = 'Good Morning';
+// // console.log(greeting + ' ' + name);
+
+// let html;
+// html  = "<h1> this is heading</h1>"+
+//         "<p> this is My para</p>";
+
+// html = html.concat('this', ' str2');
+// console.log(html);
+// // console.log(html.length);
+// // console.log(html.toLowerCase());
+// // console.log(html.toUpperCase());
+// // console.log(html);
+
+
+// // console.log(html[1]);
+// // console.log(html.indexOf('<'));
+// // console.log(html.lastIndexOf('<'));
+// // console.log(html.charAt(3));
+// // console.log(html.endsWith('dsfsdfd'));
+// // console.log(html.includes(' fg'));
+// // console.log(html.substring(1,6));
+// // console.log(html.slice(0, 4))
+  
+   //console.log(html.split(' '));
+  
+// // console.log(html.replace('this', 'it'));
+
+// let fruit1 = 'Orange\'';
+// let fruit2 = 'Apple';
+// let myHtml = `Hello ${name}
+//             <h1> This is "my" heading </h1>
+//             <p> You like ${fruit1} and ${fruit2}`;
+
+// document.body.innerHTML = myHtml;            
+
+
